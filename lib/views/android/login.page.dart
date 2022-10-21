3d506c46ae83034d43f9f8d34e8c593faaf95d10
RubberEyes <ninovello2000@gmail.com>
2022-10-21 16:06:22 -0300

    fdsfgs

--- lib/views/android/login.page.dart
+++ lib/views/android/login.page.dart
@@ -2,22 +2,28 @@ import 'package:flutter/material.dart';
 //import models as needed
 
 class LoginPage extends StatelessWidget {
-  var estrela_icon = 'image/icon.png';
+  var estrela_icon = 'images/S5.png';
+  double? icon_size = 200;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         //#46b3eb
-        backgroundColor: Color.fromARGB(255, 70, 179, 235),
+        backgroundColor: const Color.fromARGB(255, 70, 179, 235),
         body: Container(
           alignment: Alignment.center,
-          margin: EdgeInsets.all(5),
+          margin: const EdgeInsets.all(40),
           child: Column(
             children: [
-              Text(
+              const Text(
                 "Estrelas de Aluguel",
-                style: TextStyle(fontSize: 50),
+                style: TextStyle(fontSize: 32, color: Colors.white),
               ),
-              Image.asset(estrela_icon),
+              Image.asset(
+                estrela_icon,
+                fit: BoxFit.fill,
+                width: icon_size,
+                height: icon_size,
+              ),
               Text("E-mail"),
               TextField(),
               Text("Senha"),
