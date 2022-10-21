770d97b635d0f967c0e5577e30189d04d465d602
iJeanMC <jeanmontija_@hotmail.com>
2022-10-20 21:34:23 -0300

    Model

---
+++ lib/models/user.model.dart
@@ -0,0 +1,22 @@
+class User
+{
+  int? id; 
+  String? username;
+  String? password_hash;
+
+  User(this.username, this.password_hash);
+
+  User.fromMap(Map<dynamic, dynamic> map) {
+    id = map['id'];
+    username = map['username'];
+    password_hash = map['password_hash'];
+  }
+
+    Map<String, dynamic> toMap() {    
+    return {
+      'id': id,
+      'username': username,
+      'password_hash': password_hash
+    };
+  }
+}
