e6d506b884d5dd1dd2e62e89d3064c44b107dcb8
iJeanMC <jeanmontija_@hotmail.com>
2022-11-07 13:36:27 -0300

    models/Controllers

---
+++ lib/controllers/review.controller.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart';
+import '../models/review.model.dart';
+
+class Worker_TagsController {
+  Future<Database> getDatabase() async {
+    var path = await getDatabasesPath();
+    path += 'uservice.db';
+
+    Database database = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute(
+          'CREATE TABLE review (id INTEGER PRIMARY KEY autoincrement, stars INTEGER NOT NULL, text_box TEXT NOT NULL, title TEXT NOT NULL, authorid INTEGER NOT NULL, FOREIGN KEY (authorid) REFERENCES user(id), worker_reviewedid INTEGER NOT NULL, FOREIGN KEY (worker_reviewedid) REFERENCES worker(id))');
+    });
+
+    return database;
+  }
+
+  Future<void> create(Review review) async {
+    var database = await getDatabase();
+    await database.insert('Review', review.toMap());
+    await database.close();
+  }
+
+  Future<List<Map>> read() async {
+    var database = await getDatabase();
+    List<Map> reviews = await database.rawQuery('SELECT * FROM review');
+    await database.close();
+    return reviews;
+  }
+
+  Future<List<Map>> search(String value) async {
+    var database = await getDatabase();
+    List<Map> reviews =
+        await database.rawQuery("SELECT * FROM review WHERE id");
+    await database.close();
+    return reviews;
+  }
+
+  Future<void> update(Review review) async {
+    var database = await getDatabase();
+    await database.update('Review', review.toMap(),
+        where: 'id = ?', whereArgs: [review.id]);
+    await database.close();
+  }
+
+/*  Future<void> delete(Task task) async {
+    var database = await getDatabase();
+    await database.delete('Task', where: 'id = ?', whereArgs: [task.id]);
+    await database.close();
+  } */
+}
